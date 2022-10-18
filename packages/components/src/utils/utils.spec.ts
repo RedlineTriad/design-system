987967ab3a6551a81dc1e079766474da5420aea3
Oliver Schürch <oliver.schuerch@post.ch>
2022-10-18 10:18:23 +0200

    test(components): add unit test for formatPropValueList utility

--- packages/components/src/utils/utils.spec.ts
+++ packages/components/src/utils/utils.spec.ts
@@ -1,4 +1,4 @@
-import { format } from './utils';
+import { format, formatPropValueList } from './utils';
 
 describe('format', () => {
   it('returns empty string for no names defined', () => {
@@ -15,5 +15,25 @@ describe('format', () => {
 
   it('formats first, middle and last names', () => {
     expect(format('Joseph', 'Quincy', 'Publique')).toEqual('Joseph Quincy Publique');
+  });
+});
+
+describe('formatPropValueList', () => {
+  it('returns empty string for no or empty array input', () => {
+    expect(formatPropValueList()).toEqual('');
+    expect(formatPropValueList([])).toEqual('');
+  });
+
+  it('format one value', () => {
+    expect(formatPropValueList(['primary'])).toEqual('<code>primary</code>');
+  });
+
+  it('format two values', () => {
+    expect(formatPropValueList(['primary', 'secondary'])).toEqual('<code>primary</code> or <code>secondary</code>');
+  });
+
+  it('format three or more values', () => {
+    expect(formatPropValueList(['primary', 'secondary', 'tertiary'])).toEqual('<code>primary</code>, <code>secondary</code> or <code>tertiary</code>');
+    expect(formatPropValueList(['primary', 'secondary', 'tertiary', 'quaternary'])).toEqual('<code>primary</code>, <code>secondary</code>, <code>tertiary</code> or <code>quaternary</code>');
   });
 });
--- packages/components/src/utils/utils.ts
+++ packages/components/src/utils/utils.ts
@@ -2,7 +2,7 @@ export function format(first: string, middle: string, last: string): string {
   return (first || '') + (middle ? ` ${middle}` : '') + (last ? ` ${last}` : '');
 }
 
-export function formatPropValueList (values: string[], allowAllValues: boolean = false) {
+export function formatPropValueList (values: string[] = [], allowAllValues: boolean = false) {
   let valueList = values;
   
   if (!allowAllValues) valueList = valueList.filter(v => typeof v === 'string' && v !== '');
